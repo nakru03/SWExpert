2bbbf151560ad70e3f077c4a59f0e902052a45b1
SangHyung Kim <nakru03@gmail.com>
2019-08-22 07:59:16 +0900

    [Solve] 1266.java

--- Solution1266.java
+++ Solution1266.java
@@ -11,21 +11,18 @@ public class Solution1266 {
 		int tc = Integer.parseInt(br.readLine());
 		for(int i=0; i<tc; ++i) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int skillofA = Integer.parseInt(st.nextToken());
-			int skillofB = Integer.parseInt(st.nextToken());
+			double perA = (double)Integer.parseInt(st.nextToken())/100;
+			double perB = (double)Integer.parseInt(st.nextToken())/100;
 			
-			double perA = (double)skillofA/100;
-			double perB = (double)skillofB/100;
-			
-			System.out.println(perA +" "+perB);
-			double sum =0.0;
+			double sum1 =0.0;
+			double sum2=0.0;
 			for(int j=0; j<7; ++j) {
-				sum += (double)combi[j] * Math.pow(perA,prime[j]) * Math.pow(1-perA, 18-prime[j]);
+				sum1 += (double)combi[j] * Math.pow(perA,prime[j]) * Math.pow(1-perA, 18-prime[j]);
 			}
 			for(int j=0; j<7; ++j) {
-				sum += (double)combi[j] * Math.pow(perB,prime[j]) * Math.pow(1-perB, 18-prime[j]);
+				sum2 += (double)combi[j] * Math.pow(perB,prime[j]) * Math.pow(1-perB, 18-prime[j]);
 			}
-			System.out.println(sum);
+			System.out.printf("#%d %6f\n",(i+1),sum1+sum2-(sum1*sum2)); //벤다이어그램으로 생각. 교집합을 빼줘야됨.
 		}
 				
 	}
